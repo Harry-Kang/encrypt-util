82a1123d969b8b856c1dd83ac1b0c15f6c3356a2
康茜 <xikang2@creditease.cn>
2021-05-14 18:48:08 +0800

    add aes/ecb crypt mode

--- aesutil/aesutil.go
+++ aesutil/aesutil.go
@@ -5,11 +5,13 @@ import (
 	"crypto/aes"
 	"crypto/cipher"
 	"errors"
+	"github.com/intrsokx/encrypt-util/aesutil/ecb"
 )
 
 //default iv
 var IV = []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f}
 
+//default encrypt is cfb encrypted
 func Encrypt(origData, key, iv []byte) ([]byte, error) {
 	block, err := aes.NewCipher(key)
 	if err != nil {
@@ -132,4 +134,35 @@ func ZeroUnPadding(cipherData []byte) []byte {
 		func(r rune) bool {
 			return r == rune(0)
 		})
+}
+
+func ECBEncrypt(origData, key []byte, padingFun PadingFunc) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	//加密（1、先填充数据。 2、再加密）
+	origData = padingFun(origData)
+	cipherText := make([]byte, len(origData))
+
+	ecbEncrypter := ecb.NewECBEncrypter(block)
+	ecbEncrypter.CryptBlocks(cipherText, origData)
+
+	return cipherText, nil
+}
+
+func ECBDecrypt(cipheredData, key []byte, unPadingFun PadingFunc) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	//解密（1、先解密。 2、再去除填充数据）
+	ecbDecrypter := ecb.NewECBDencrypter(block)
+
+	plainText := make([]byte, len(cipheredData))
+	ecbDecrypter.CryptBlocks(plainText, cipheredData)
+
+	return unPadingFun(plainText), nil
 }
